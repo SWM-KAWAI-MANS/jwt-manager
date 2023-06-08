09731593327880d3bac7294404571a35e10a4e3d
Hyeonjun Park <phjppo0918@gmail.com>
2023-06-08 15:33:32 +0900

    feat : initial setting

---
+++ src/main/java/online/partyrun/jwtmanager/JwtManagerApplication.java
@@ -0,0 +1,13 @@
+package online.partyrun.jwtmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/partyrun/jwtmanager/JwtManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package online.partyrun.jwtmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
