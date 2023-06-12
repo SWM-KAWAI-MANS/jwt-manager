abd704722bcf546601b60b2bb5fb7a2b636d020f
Hyeonjun Park <phjppo0918@gmail.com>
2023-06-12 13:40:10 +0900

    feat : exception 발생 시 로그 출력 기능 추가 #PAR-61

--- src/main/java/online/partyrun/jwtmanager/controller/JwtControllerAdvice.java
+++ src/main/java/online/partyrun/jwtmanager/controller/JwtControllerAdvice.java
@@ -1,21 +1,20 @@
 package online.partyrun.jwtmanager.controller;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
-import io.jsonwebtoken.security.SecurityException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @AutoConfiguration
 @RestControllerAdvice(basePackages = "online.partyrun.jwtmanager")
 public class JwtControllerAdvice {
     @ExceptionHandler({JwtException.class, IllegalArgumentException.class})
     public ResponseEntity<ExceptionResponse> handleJwtException(RuntimeException exception) {
+        log.error("{}", exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ExceptionResponse("승인되지 않은 요청입니다."));
     }
 }
