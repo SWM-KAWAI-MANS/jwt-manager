9f100baeb0aa6b7851b87d9d57475923886c9431
seongwoo <seongwoo0513@uos.ac.kr>
2023-09-05 12:43:58 +0900

    feat: basepackage 제거

--- src/main/java/online/partyrun/jwtmanager/controller/JwtControllerAdvice.java
+++ src/main/java/online/partyrun/jwtmanager/controller/JwtControllerAdvice.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice(basePackages = "online.partyrun.jwtmanager")
+@RestControllerAdvice
 public class JwtControllerAdvice {
 
     @PostConstruct
